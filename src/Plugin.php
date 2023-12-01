974f3ed51d889af0a57d53e11fad7eb83dc7c71b
osterhasemops <osterhasemops@gmail.com>
2023-12-01 10:18:40 +0300

    Update configure project

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,19 +8,35 @@ use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Factory;
 use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
 use Composer\Json\JsonManipulator;
-use Composer\Plugin\PluginEvents;
+use Composer\Package\Locker;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\ScriptEvents;
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
-    private JsonManipulator $manipulator;
+    /**
+     * @var JsonManipulator
+     */
+    private $manipulator;
+
+    /**
+     * @var Composer
+     */
+    private $composer;
+
+    /**
+     * @var IOInterface
+     */
+    private $io;
 
     public function activate(Composer $composer, IOInterface $io)
     {
         $composerFile = Factory::getComposerFile();
         $this->manipulator = new JsonManipulator(file_get_contents($composerFile));
+        $this->composer = $composer;
+        $this->io = $io;
     }
 
     public function deactivate(Composer $composer, IOInterface $io)
@@ -44,6 +60,20 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     public function configureProject(): void
     {
         $this->manipulator->addMainKey('extra', ['hooks' => ['pre-commit' => 'echo test']]);
+
+        file_put_contents(Factory::getComposerFile(), $this->manipulator->getContents());
+
+        $this->updateComposerLock();
     }
 
+    private function updateComposerLock()
+    {
+        $lock = substr(Factory::getComposerFile(), 0, -4).'lock';
+        $composerJson = file_get_contents(Factory::getComposerFile());
+        $lockFile = new JsonFile($lock, null, $this->io);
+        $locker = new Locker($this->io, $lockFile, $this->composer->getInstallationManager(), $composerJson);
+        $lockData = $locker->getLockData();
+        $lockData['content-hash'] = Locker::getContentHash($composerJson);
+        $lockFile->write($lockData);
+    }
 }
