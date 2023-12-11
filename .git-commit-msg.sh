77f503fc839bee3436dd4f2d8a77948599b3e26f
osterhasemops <osterhasemops@gmail.com>
2023-12-11 15:40:29 +0300

    Fix commit message script

--- .git-commit-msg.sh
+++ .git-commit-msg.sh
@@ -12,7 +12,7 @@ fi
 
 echo -e -n "\033[1;33mCheck message format: \033[0m";
 
-egrep -q -e '^[a-zA-Z0-\]+.*?[[:space:]].+$' $1; ERROR=$?;
+egrep -q -e '^[a-zA-Z0-9\]+.*?[[:space:]].+$' $1; ERROR=$?;
 if [ $ERROR -ne 0 ]; then
     echo -e "\033[0;31mNOT MATCH\033[0m";
     exit $ERROR;
@@ -22,7 +22,7 @@ fi
 
 echo -e -n "\033[1;33mCheck branch vs message: \033[0m";
 
-COMMIT_BRANCH=`egrep -o -e '^[A-Z]+-[0-9]+' $1`
+COMMIT_BRANCH=`egrep -o -e '^[a-zA-Z0-9\]+' $1`
 
 egrep -q -e "^$COMMIT_BRANCH" <<< "$LOCAL_BRANCH"; ERROR=$?;
 if [ $ERROR -ne 0 ]; then
